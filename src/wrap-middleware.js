41894286ee8dd650b15e76c64cf71efdf08671ef
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-01-29 13:39:48 +0100

    If an error is thrown while calling the WS middleware, let's next that

--- src/wrap-middleware.js
+++ src/wrap-middleware.js
@@ -2,8 +2,12 @@ export default function wrapMiddleware(middleware) {
   return (req, res, next) => {
     if (req.ws !== null && req.ws !== undefined) {
       req.wsHandled = true;
-      /* Unpack the `.ws` property and call the actual handler. */
-      middleware(req.ws, req, next);
+      try {
+        /* Unpack the `.ws` property and call the actual handler. */
+        middleware(req.ws, req, next);
+      } catch (err) {
+        next(err);
+      }
     } else {
       /* This wasn't a WebSocket request, so skip this middleware. */
       next();
